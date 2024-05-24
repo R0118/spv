c2623b783093f69f9c7a6c7cbd75c774ada9905f
R0118 <01187770@pw.edu.pl>
2024-05-24 22:33:53 +0200

    c

--- sp_vector.h
+++ sp_vector.h
@@ -6,9 +6,15 @@
 template <typename T>
 class sparse_vector
 {
-    struct vect_elem {
-        size_t  idx;
-        T       value;
+    struct vect_elem
+    {
+        size_t idx;
+        T value;
+
+        bool operator<(const vect_elem &other) const
+        {
+            return idx < other.idx;
+        }
     };
 
     size_t n_elem = 0;
@@ -43,67 +49,65 @@ public:
     // invalid idx results in invalid_argument exception
     T operator[](int idx) const
     {
-        if (idx < 0 || size() <= (size_t) idx)
+        if (idx < 0 || size() <= (size_t)idx)
+        {
             throw std::invalid_argument("Const index out of bounds.");
+        }
 
-        auto it = std::find_if(elements.cbegin(), elements.cend(), [idx](const vect_elem& ve) { return ve.idx == (size_t) idx; });
-        if (it == elements.cend())
+        auto it = std::lower_bound(elements.cbegin(), elements.cend(), vect_elem{(size_t)idx, T()});
+        if (it == elements.cend() || it->idx != (size_t)idx)
+        {
             return T();
+        }
         return it->value;
     }
 
     // returns const reference to element in the vector
     // if element with given idx has default value new element is inserted
     // invalid idx results in invalid_argument exception
-    T& operator[](int idx)
+    T &operator[](int idx)
     {
-        if (idx < 0 || size() <= (size_t) idx)
-            throw std::invalid_argument("Index out of bounds.");
-
-        auto it = std::find_if(elements.begin(), elements.end(), [idx](const vect_elem& ve) { return ve.idx == (size_t) idx; });
-        if (it == elements.cend())
+        if (idx < 0 || size() <= (size_t)idx)
         {
-            elements.push_back({(size_t) idx, T()});
-            return elements.back().value;
+            throw std::invalid_argument("Index out of bounds.");
+        }
+
+auto it = std::lower_bound(elements.begin(), elements.end(), vect_elem{(size_t)idx, T()});
+        if (it == elements.end() || it->idx != (size_t)idx) {
+            it = elements.insert(it, {(size_t)idx, T()});
         }
         return it->value;
     }
-
     class const_iterator
     {
-        const sparse_vector& container;
-        size_t current_idx = 0;
+        typename std::vector<vect_elem>::const_iterator it;
 
-        const_iterator(const sparse_vector& container, size_t index) :
-            container(container), current_idx(index)
-        {}
-        friend sparse_vector;
     public:
-        // Please define minimal interface for forward iterator
-        const_iterator& operator++()
+        const_iterator(typename std::vector<vect_elem>::const_iterator it) : it(it) {}
+
+        const_iterator &operator++()
         {
-            ++current_idx;
+            ++it;
             return *this;
         }
-        bool operator!=(const const_iterator& other)
+        bool operator!=(const const_iterator &other)
         {
-            return &container != &other.container || current_idx != other.current_idx;
+            return it != other.it;
         }
         T operator*() const
         {
-            return container[current_idx];
+            return it->value;
         }
     };
 
     const_iterator cbegin() const
     {
-        return const_iterator(*this, 0);
+        return const_iterator(elements.cbegin());
     }
     const_iterator cend() const
     {
-        return const_iterator(*this, size());
+        return const_iterator(elements.cend());
     }
-
     const_iterator begin() const
     {
         return cbegin();
@@ -113,11 +117,13 @@ public:
         return cend();
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const sparse_vector& vec)
+    friend std::ostream &operator<<(std::ostream &os, const sparse_vector &vec)
     {
-        os << '[';
-        for (auto val : vec)
-            os << val << ' ';
-        return os << ']';
+       os << "[";
+        for (size_t i = 0; i < vec.size(); ++i)
+        {
+            os << vec[i] << " ";
+        }
+        return os << "]";
     }
 };
